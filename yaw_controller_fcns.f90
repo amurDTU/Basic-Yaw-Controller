aa3ece3538588da7ada0799c6685086ceab790b5
amurDTU <35494132+amurDTU@users.noreply.github.com>
2018-10-23 15:13:52 +0200

    code cleaned

--- yaw_controller_fcns.f90
+++ yaw_controller_fcns.f90
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module yaw_controller_fcns
 !use misc_mod
 ! Types
@@ -21,28 +20,4 @@ type(yaw_str) yawst
 !*****************************************************************************************
 contains
 !*****************************************************************************************
-=======
-module yaw_controller_fcns
-!use misc_mod
-! Types
-  integer, parameter :: mk = kind(1.0d0)
-  real(mk) pi, degrad, raddeg
-   parameter(pi = 3.14159265358979_mk, degrad = 0.01745329251994_mk, raddeg = 57.295779513093144_mk)
-type yaw_str
-
-  real*8 :: tstart
-  integer :: larray , larray2
-  real*8 :: threshold, lastyaw, ct , memory
-  real*8 :: flagyaw
-  real(mk) :: array4(100000), arrayaux(1,100000)
-  
-end type yaw_str
-
-type(yaw_str) yawst
-
-
-!*****************************************************************************************
-contains
-!*****************************************************************************************
->>>>>>> 269147ab605c969219a7b16accb5b933f4d8a673
 end module yaw_controller_fcns
